8d78e8575bcb86c85a59f8169b503e4d1dbfc627
aguilarchris0623 <aguilar.chris0623@gmail.com>
2026-08-12 22:36:12 -0400

    Update ex3_cen.py

--- ex3_cen.py
+++ ex3_cen.py
@@ -18,9 +18,11 @@ min_income = int(df['Median household income'].min())
 max_income = int(df['Median household income'].max())
 
 income_range = st.slider(
-    'Select a Median Household Income Range',
     min_value=min_income,
     max_value=max_income,
-    value=(min_income, max_income))
+
+income_filtered_df = df[
+    (df['Median household income'] >= income_range[0]) &
+    (df['Median household income'] <= income_range[1])]
 
 st.dataframe(income_filtered_df[['Place', 'Median household income']], width=800, height=200)
