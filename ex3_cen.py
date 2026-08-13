d5d61ca293c6482a1d03fef6f9486226fbd46d34
aguilarchris0623 <aguilar.chris0623@gmail.com>
2026-08-12 22:55:58 -0400

    Update ex3_cen.py

--- ex3_cen.py
+++ ex3_cen.py
@@ -32,8 +32,17 @@ st.dataframe(income_filtered_df[['Place', 'Median household income']], width=800
 df_sorted = df.sort_values(by='Median household income', ascending=False)
 top_5_income = df_sorted.head(5)
 bottom_5_income = df_sorted.tail(5)
+
+# Combine for plotting
 plot_df = pd.concat([top_5_income, bottom_5_income])
+
+# Create the bar chart using Altair for better Streamlit integration and interactivity
 chart = alt.Chart(plot_df).mark_bar().encode(
-x=alt.X('Town:N', sort=alt.EncodingSortField(field='Median household income', op='mean', order='descending'), title='City Name'),
-y=alt.Y('Median household income:Q', title='Median Household Income'))
+    x=alt.X('Town:N', sort=alt.EncodingSortField(field='Median household income', op='mean', order='descending'), title='City Name'),
+    y=alt.Y('Median household income:Q', title='Median Household Income'),
+    color=alt.Color('Median household income:Q', scale=alt.Scale(range='heatmap')) # Color by income level
+).properties(
+    title='Top 5 Highest and Bottom 5 Lowest Median Household Income Cities'
+)
+
 st.altair_chart(chart, use_container_width=True)
