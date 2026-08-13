f1545d31a17d794aad01685c42983104de50c774
aguilarchris0623 <aguilar.chris0623@gmail.com>
2026-08-12 22:30:14 -0400

    Update ex3_cen.py

--- ex3_cen.py
+++ ex3_cen.py
@@ -14,21 +14,15 @@ selected_county = st.selectbox('Select a County', counties)
 county_df = df[df['County'] == selected_county]
 st.dataframe(county_df[['Place']], width=800, height=200)
 
-min_income = int(df['Median household income'].min())
-max_income = int(df['Median household income'].max())
-
 income_range = st.slider(
     'Select a Median Household Income Range',
     min_value=min_income,
     max_value=max_income,
-    value=(min_income, max_income) # Default to full range
-)
+    value=(min_income, max_income))
 
-# Filter DataFrame based on selected income range
 income_filtered_df = df[
     (df['Median household income'] >= income_range[0]) &
-    (df['Median household income'] <= income_range[1])
-]
+    (df['Median household income'] <= income_range[1])]
 
 st.subheader(f'Towns with Median Household Income between ${income_range[0]:,} and ${income_range[1]:,}')
 st.dataframe(income_filtered_df[['Town', 'Median household income']], width=800, height=200)
