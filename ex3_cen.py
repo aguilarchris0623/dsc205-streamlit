24ac369b5952452ce03ccf434430088feb4d2891
aguilarchris0623 <aguilar.chris0623@gmail.com>
2026-08-12 22:45:48 -0400

    Update ex3_cen.py

--- ex3_cen.py
+++ ex3_cen.py
@@ -18,7 +18,7 @@ min_income = int(df['Median household income'].min())
 max_income = int(df['Median household income'].max())
 
 income_range = st.slider(
-    'Select a Income Range'
+    'Select a Income Range',
     min_value=min_income,
     max_value=max_income,
     value=(min_income, max_income))
