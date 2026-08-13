ff62aa7a4a6ec4fa1887ff0b07811a3c5ea8420f
aguilarchris0623 <aguilar.chris0623@gmail.com>
2026-08-12 23:10:56 -0400

    Update ex3_cen.py

--- ex3_cen.py
+++ ex3_cen.py
@@ -35,7 +35,7 @@ bottom_5_income = df_sorted.tail(5)
 
 plot_df = pd.concat([top_5_income, bottom_5_income])
 
-fig, ax = plt.subplots(figsize=(20, 6))
+fig, ax = plt.subplots(figsize=(15, 8))
 ax.bar(plot_df['Place'], plot_df['Median household income'])
 ax.set_xlabel('City Name')
 ax.set_ylabel('Median Household Income')
