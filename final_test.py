e7e7a00032bba48de009bbe19a3d4fc8ee416735
aguilarchris0623 <aguilar.chris0623@gmail.com>
2026-08-15 19:50:32 -0400

    Update final_test.py

--- final_test.py
+++ final_test.py
@@ -38,8 +38,8 @@ latest_date = df['Last update date'].max()
 latest_df = df[df['Last update date'] == latest_date].copy()
 
 # Calculate per-capita metrics
-latest_df['Deaths per 100k'] = (latest_df.dropna().['Total deaths'] / latest_df.dropna().['Population']) * 100000
-latest_df['Positivity Rate (%)'] = (latest_df.dropna().['Number_of_positives'] / latest_df.dropna().['Number_of_tests']) * 100
+latest_df['Deaths per 100k'] = (latest_df.['Total deaths'] / latest_df.['Population']) * 100000
+latest_df['Positivity Rate (%)'] = (latest_df.['Number_of_positives'] / latest_df.['Number_of_tests']) * 100
 
 # Widget: Metric Selection Dropdown
 selected_metric = st.selectbox(
