672f22750c9502bfacb9d067ea9fd08bace87fc6
aguilarchris0623 <aguilar.chris0623@gmail.com>
2026-08-15 18:29:50 -0400

    Update Final.py

--- Final.py
+++ Final.py
@@ -1,1 +1,28 @@
-final
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv("covid_tests.csv")
+df2 = pd.read_csv("2020v2021ct.csv")
+
+df['Last update date'] = pd.to_datetime(df['Last update date'])
+df['YearMonth'] = df['Last update date'].dt.to_period('M')
+columns_to_sum = [
+    'Total cases', 'Confirmed cases', 'Probable cases',
+    'Total deaths', 'Confirmed deaths', 'Probable deaths',
+    'People tested', 'Number of tests', 'Number of positives',
+    'Number of negatives', 'Number of indeterminates']
+
+# Clean column names by stripping whitespace
+df.columns = df.columns.str.strip()
+
+# Convert specified columns to numeric, handling non-numeric values by coercing them to NaN
+for col in columns_to_sum:
+    if col in df.columns:
+        df[col] = df[col].astype(str).str.replace(',', '', regex=False)
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+
+# Group by 'Town' and 'YearMonth' and sum the relevant columns
+monthly_updates = df.groupby(['Town', 'YearMonth'])[columns_to_sum].sum().reset_index()
+
+df2 = pd.read_csv("2020v21CT.csv")
