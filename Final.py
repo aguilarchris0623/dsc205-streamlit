ef7ff73b5017a5841aba3c4c983636f7f59628f5
aguilarchris0623 <aguilar.chris0623@gmail.com>
2026-08-15 18:50:18 -0400

    Update Final.py

--- Final.py
+++ Final.py
@@ -6,31 +6,29 @@ df_tests = pd.read_csv("https://raw.githubusercontent.com/aguilarchris0623/dsc20
 df_pop = pd.read_csv('https://raw.githubusercontent.com/aguilarchris0623/dsc205-streamlit/refs/heads/main/2020v21ct.csv')
 
 # Aggregate total population per town & assign tiers
-    town_pop = (
-        df_pop.groupby('TOWN NAME')['ALL_RACE-ETHN'].sum().reset_index()
-    )
-    town_pop['Town'] = (
-        town_pop['TOWN NAME'].str.replace(' town', '', case=False).str.strip()
-    )
-    town_pop = town_pop.rename(columns={'ALL_RACE-ETHN': 'Population'})
+town_pop = (
+df_pop.groupby('TOWN NAME')['ALL_RACE-ETHN'].sum().reset_index())
+town_pop['Town'] = (
+town_pop['TOWN NAME'].str.replace(' town', '', case=False).str.strip())
+town_pop = town_pop.rename(columns={'ALL_RACE-ETHN': 'Population'})
 
-    def get_tier(pop):
-        if pop > 50000:
-            return 'Urban Hubs (>50k)'
-        elif pop >= 10000:
-            return 'Suburban (10k-50k)'
-        else:
-            return 'Rural (<10k)'
+def get_tier(pop):
+    if pop > 50000:
+        return 'Urban Hubs (>50k)'
+    elif pop >= 10000:
+        return 'Suburban (10k-50k)'
+    else:
+        return 'Rural (<10k)'
 
-    town_pop['Town_Tier'] = town_pop['Population'].apply(get_tier)
+town_pop['Town_Tier'] = town_pop['Population'].apply(get_tier)
 
-    # Merge population demographics into Covid dataset
-    df = pd.merge(
-        df_tests,
-        town_pop[['Town', 'Population', 'Town_Tier']],
-        on='Town',
-        how='inner',)
-    return df
+# Merge population demographics into Covid dataset
+df = pd.merge(
+df_tests,
+town_pop[['Town', 'Population', 'Town_Tier']],
+on='Town',
+how='inner',)
+return df
 
 
 
