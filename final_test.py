7df2ce81fd18eb2db82b650eed7d5c22d2f28298
aguilarchris0623 <aguilar.chris0623@gmail.com>
2026-08-15 23:00:14 -0400

    Update final_test.py

--- final_test.py
+++ final_test.py
@@ -35,9 +35,8 @@ metric_choice = st.selectbox(
         "Metric",
         ["Deaths per 100k", "Total deaths", "Positivity Rate (%)"],)
 
-latest = (data.sort_values("Last update date").groupby("Town", as_index=False).last())
-latest["Deaths per 100k"] = (latest["Total deaths"] / latest["Population"]) * 100_000
-latest["Positivity Rate (%)"] = (latest["Number of positives"] / latest["Number of tests"]) * 100
+df["Deaths per 100k"] = (df["Total deaths"] / df["Population"]) * 100_000
+df["Positivity Rate (%)"] = (df["Number of positives"] / df["Number of tests"]) * 100
 
 fig1 = px.scatter(
         latest,
