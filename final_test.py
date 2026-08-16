74149f49cd7c37187aaf270089d2b05466e9b671
aguilarchris0623 <aguilar.chris0623@gmail.com>
2026-08-16 16:04:55 -0400

    Update final_test.py

--- final_test.py
+++ final_test.py
@@ -19,9 +19,10 @@ columns_to_exclude = ['Last update date', 'Town']
 
 for col in df_tests.columns:
     if col not in columns_to_exclude:
-        if df_tests[col].dtype == 'object':
-            df_tests[col] = df_tests[col].astype(str).str.replace(',', '', regex=False)
-        df_tests[col] = pd.to_numeric(df_tests[col], errors='coerce')
+        if df_tests[col].equals(pd.to_numeric(
+                df_tests[col].astype(str).str.replace(",", "", regex=False),
+                errors="coerce")):
+                    pass
 
 
 def get_tier(pop):
