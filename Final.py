931ac03ee0ef35741dda1263ed3bfe6fa2d78a18
aguilarchris0623 <aguilar.chris0623@gmail.com>
2026-08-16 21:28:15 -0400

    Update Final.py

--- Final.py
+++ Final.py
@@ -114,6 +114,7 @@ alpha = pd.to_datetime('2020-12-24')
 fig2.add_vline(
         x=alpha,
         line_dash="dash",
+        line_width=2,
         line_color="yellow",
         annotation_text="Aplha")
 
@@ -121,6 +122,7 @@ omnicron = pd.to_datetime('2022-01-10')
 fig2.add_vline(
         x=omnicron,
         line_dash="dash",
+        line_width=1,
         line_color="yellow",
         annotation_text="Omicron")
 
